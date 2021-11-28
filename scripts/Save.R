a7981a4deb10a5d733a606c60a311bbab8878142
irwingss <irwingssaldu@gmail.com>
2021-11-27 22:00:10 -0500

    slide4

--- scripts/Save.R
+++ scripts/Save.R
@@ -1,2 +1,2 @@
-pagedown::chrome_print("https://irwingss.github.io/R-IIAP-UNAMAD/slide3/", format = "pdf", wait=30, timeout=120)
+pagedown::chrome_print("https://irwingss.github.io/R-IIAP-UNAMAD/slide4/", format = "pdf", wait=30, timeout=120)
   
